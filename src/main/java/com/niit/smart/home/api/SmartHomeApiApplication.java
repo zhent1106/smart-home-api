276561298692dd18bd603bd2847393f104be6d77
zhent1106 <1299088269@qq.com>
2020-07-06 23:15:23 +0800

    架子搭建

---
+++ src/main/java/com/niit/smart/home/api/SmartHomeApiApplication.java
@@ -0,0 +1,13 @@
+package com.niit.smart.home.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartHomeApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SmartHomeApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/niit/smart/home/api/SmartHomeApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.niit.smart.home.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartHomeApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
